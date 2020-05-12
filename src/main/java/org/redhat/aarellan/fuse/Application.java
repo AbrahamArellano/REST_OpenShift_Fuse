3565a230b37eb822da35531960a116e016a92934
aarellan <aarellan@view-localhost>
2020-05-12 14:34:40 +0200

    Initial commit

---
+++ src/main/java/org/redhat/aarellan/fuse/Application.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2005-2016 Red Hat, Inc.
+ *
+ * Red Hat licenses this file to you under the Apache License, version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.  See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package org.redhat.aarellan.fuse;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
+public class Application extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Component
+    class RestApi extends RouteBuilder {
+    	
+    	OrderService service = new OrderService();
+
+        @Override
+        public void configure() {
+            restConfiguration()
+                .contextPath("/camel-rest").apiContextPath("/api")
+                    .apiProperty("api.title", "Camel REST API")
+                    .apiProperty("api.version", "1.0")
+                    .apiProperty("cors", "true")
+                    .apiProperty("api.specification.contentType.json", "application/vnd.oai.openapi+json;version=2.0")
+                    .apiProperty("api.specification.contentType.yaml", "application/vnd.oai.openapi;version=2.0")
+                    .apiContextRouteId("doc-api")
+                .component("servlet")
+                .bindingMode(RestBindingMode.json);
+
+            rest("/data").description("Simple REST service")
+                .get("/").description("My Request")
+                    .route().routeId("simple-api")
+                    .bean(service, "generateOrder")
+                    .to("log:org.redhat.aarellan.fuse")
+                    .endRest();
+        }
+    }
+}

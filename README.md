6b4c2608654c284d3f5f5232ce6d386d8d2552b7
aarellan <aarellan@view-localhost>
2020-05-13 13:38:22 +0200

    Stable version - netty4-http - with connection headers

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Camel REST application deployed on OpenShift using Fuse
 ## Prerequisites
 Please read and follow the instructions on README_OCP_Preparation.md
 
-## Create project
+## Create project (only as a reference - do not use this step)
 mvn org.apache.maven.plugins:maven-archetype-plugin:2.4:generate \
   -DarchetypeCatalog=https://maven.repository.redhat.com/ga/io/fabric8/archetypes/archetypes-catalog/2.2.0.fuse-sb2-760038-redhat-00001/archetypes-catalog-2.2.0.fuse-sb2-760038-redhat-00001-archetype-catalog.xml \
   -DarchetypeGroupId=org.jboss.fuse.fis.archetypes \
@@ -20,3 +20,40 @@ mvn fabric8:deploy -Popenshift
 
 ## Undeploy OpenShift
 mvn fabric8:undeploy -Popenshift
+
+
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-undertow</artifactId>
+		</dependency>
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-actuator</artifactId>
+		</dependency>
+		
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-web</artifactId>
+			<exclusions>
+				<exclusion>
+					<groupId>org.springframework.boot</groupId>
+					<artifactId>spring-boot-starter-tomcat</artifactId>
+				</exclusion>
+			</exclusions>
+		</dependency>		
+		
+		<dependency>
+			<groupId>org.springframework.boot</groupId>
+			<artifactId>spring-boot-starter-web</artifactId>
+			<exclusions>
+				<exclusion>
+					<groupId>org.springframework.boot</groupId>
+					<artifactId>spring-boot-starter-tomcat</artifactId>
+				</exclusion>
+			</exclusions>
+		</dependency>		
+
+		<dependency>
+			<groupId>org.apache.camel</groupId>
+			<artifactId>camel-servlet-starter</artifactId>
+		</dependency>		
--- src/main/java/org/redhat/aarellan/fuse/Application.java
+++ src/main/java/org/redhat/aarellan/fuse/Application.java
@@ -15,19 +15,22 @@
  */
 package org.redhat.aarellan.fuse;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Expression;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.spring.boot.SpringBootXmlCamelContextConfigurer;
+import org.redhat.aarellan.fuse.processor.ServerProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
-public class Application extends SpringBootServletInitializer {
+public class Application extends SpringBootXmlCamelContextConfigurer {
+	
+	@Autowired
+	ServerProcessor serverProcessor;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -41,15 +44,18 @@ public class Application extends SpringBootServletInitializer {
         @Override
         public void configure() {
             restConfiguration()
-                .contextPath("/camel-rest").apiContextPath("/api")
-                .component("servlet")
+            	.host("0.0.0.0")
+            	.port(8080)
+                .contextPath("/camel-rest")
+                .component("netty4-http")
                 .bindingMode(RestBindingMode.json);
 
             rest("/data").description("Simple REST service")
                 .get("/").description("My Request")
                     .route().routeId("simple-api")
                     .bean(service, "generateOrder")
-                    .to("log:org.redhat.aarellan.fuse")
+                    .process(serverProcessor)
+                    .log("${in.headers.CamelNettyRemoteAddress}")
                     .endRest();
         }
     }
